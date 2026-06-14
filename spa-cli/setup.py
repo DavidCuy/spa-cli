72d80de5f0cbd223375388e388377e3d8ca35459
DavidCuy <david.cuy.sanchez@gmail.com>
2026-06-13 22:35:50 -0600

    feat: upgrade version

--- spa-cli/setup.py
+++ spa-cli/setup.py
@@ -25,7 +25,7 @@ entry_points = \
 
 setup_kwargs = {
     'name': 'spa-cli',
-    'version': '1.2.0',
+    'version': '2.0.0',
     'description': 'Un cli para manejar proyectos de API de python.',
     'author': 'David Cuy',
     'author_email': 'david.cuy.sanchez@gmail.com',
